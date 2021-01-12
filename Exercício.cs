c1332e4e1a9189102dcf3dc22482f80a09cf6981
Your Name <mis.monterio@hotmail.com>
2021-01-12 15:57:13 -0300

    Segundo commit

--- Exercício.cs
+++ Exercício.cs
@@ -1,15 +1,27 @@
-using System;
+using System; 
 
-    namespace curso {
-        class Program {
-        static void Main(string[] args){
-            Console.WriteLine("Qual é seu nome?: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Seu nome é: " + name);
-            Console.WriteLine("Qual a sua idade?: ");
-            int age = int.Parse(Console.ReadLine());
-            Console.WriteLine("Você tem: " + age );
-           
-        }
+class URI {
+
+    static void Main(string[] args) { 
+//Fazer um programa para ler as medidas da base e altura de um retângulo. Em
+//seguida, mostrar o valor da área, perímetro e diagonal deste retângulo, com quatro
+//casas decimais, conforme exemplo.
+
+        
+        double a, b, area, perimetro, diagonal;
+
+             
+        b = double.Parse(Console.ReadLine());
+        a = double.Parse(Console.ReadLine());
+        area = b * a;
+        perimetro = b + a + b + a;
+        diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0)); 
+        
+        Console.WriteLine("AREA = " + area.ToString("F4"));
+        Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4"));
+        Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4"));
+        
+       
     }
+
 }
